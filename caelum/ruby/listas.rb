0c51d3530b8989bab8df4516d5e2427c05f4b5f7
Adalberto Junior <adalberto.comp@gmail.com>
2014-01-16 23:12:00 -0200

    arrays

---
+++ caelum/ruby/listas.rb
@@ -0,0 +1,48 @@
+nomes = []
+
+nomes[0] = "Fazano"
+nomes << "Fogo de Chao"
+
+for nome in nomes
+	puts nome
+end
+
+
+class Franquia
+	def initialize
+		@restaurantes = []
+	end
+
+	def add(*restaurantes)
+		for restaurante in restaurantes
+			@restaurantes << restaurante
+		end
+	end
+
+	def mostra
+		for restaurante in @restaurantes
+			puts restaurante.nome
+		end
+	end
+end
+
+class Restaurante
+	attr_accessor :nome
+
+	def fechar_conta(dados)
+		puts "Conta fechada no valor de #{dados[:valor]} e com #{dados[:nota]}.
+		Comentario: #{dados[:comentario]}"
+	end
+end
+
+rest1 = Restaurante.new
+rest1.nome = "Fazano"
+rest2 = Restaurante.new
+rest2.nome = "Fogo de Chao"
+
+franquia = Franquia.new
+franquia.add rest1, rest2
+
+franquia.mostra
+
+rest1.fechar_conta :valor => 50, :nota => 9, :comentario => 'Gostei!'
